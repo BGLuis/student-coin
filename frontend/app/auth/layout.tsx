0d31f684e6e003d5eedc362ee193bf732fa4e43f
Luis Henrique <luishenrique.gbarbosa21@gmail.com>
2025-10-23 09:43:44 -0300

    feat: backgrund image

--- frontend/app/auth/layout.tsx
+++ frontend/app/auth/layout.tsx
@@ -8,7 +8,12 @@ export default function AuthLayout({
     return (
         <div className="min-h-screen flex flex-col">
             {/* Main Content */}
-            <main className="flex-grow flex items-center justify-center bg-[#f7cac9] [background-image:radial-gradient(circle_at_60%_30%,#74c6d4,rgba(116,198,212,0)_40%),radial-gradient(circle_at_20%_80%,#e85d75,rgba(232,93,117,0)_40%),radial-gradient(circle_at_90%_70%,#f5e5c5,rgba(245,229,197,0)_35%)]">
+            <main
+                className="flex-grow flex items-center justify-center bg-cover bg-center bg-no-repeat"
+                style={{
+                    backgroundImage: 'url(/image/backgrund.webp)'
+                }}
+            >
                 {children}
             </main>
 
