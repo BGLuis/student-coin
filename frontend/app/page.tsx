62b632880d6eb77609307efcd35eb63984513f27
marce <marcelacamposm2@gmail.com>
2025-10-23 16:51:42 -0300

    ajust: header

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,26 +1,14 @@
 "use client";
 
-import { useState } from "react";
-import { Button, Modal, Header, Footer } from "@/components";
+import { Header, Footer } from "@/components";
 
 export default function Home() {
-  const [isConfigOpen, setIsConfigOpen] = useState(false);
-
   return (
     <div className="flex flex-col min-h-screen" style={{ backgroundColor: '#f9f9f9' }}>
       <Header />
       
       <main className="flex flex-col items-center justify-center flex-1 gap-4 px-4">
-        <h1 className="text-4xl font-bold">Página Inicial</h1>
         
-        <Button onClick={() => setIsConfigOpen(true)}>
-          Configurações
-        </Button>
-
-        <Modal
-          isOpen={isConfigOpen}
-          onClose={() => setIsConfigOpen(false)}
-        />
       </main>
 
       <Footer />
