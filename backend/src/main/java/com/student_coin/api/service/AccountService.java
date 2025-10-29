adb17d4c9421db7b4aac96f855e7e1127ca925bd
jaoppb <joaopedroperes06@gmail.com>
2025-10-29 20:18:30 -0300

    feat(back): processing transaction

--- backend/src/main/java/com/student_coin/api/service/AccountService.java
+++ backend/src/main/java/com/student_coin/api/service/AccountService.java
@@ -1,36 +1,64 @@
 package com.student_coin.api.service;
 
 import com.student_coin.api.dto.request.RewardTransactionRequest;
+import com.student_coin.api.entity.Account;
 import com.student_coin.api.entity.RewardTransaction;
 import com.student_coin.api.entity.Student;
 import com.student_coin.api.entity.Teacher;
+import com.student_coin.api.repository.AccountRepository;
 import com.student_coin.api.repository.RewardTransactionRepository;
 import com.student_coin.api.repository.StudentRepository;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
+@Transactional
 public class AccountService {
     private final StudentRepository studentRepository;
     private final RewardTransactionRepository rewardTransactionRepository;
+    private final AccountRepository accountRepository;
+
+    private void processTransaction(Account origin, Account destination, Integer value) {
+        origin.setBalance(origin.getBalance() - value);
+        destination.setBalance(destination.getBalance() + value);
+
+        accountRepository.save(origin);
+        accountRepository.save(destination);
+    }
+
+    private RewardTransaction rollbackTransaction(RewardTransaction transaction) {
+        processTransaction(transaction.getOrigin(), transaction.getDestination(), -transaction.getValue());
+        return transaction;
+    }
+
+    private RewardTransaction processReward(RewardTransaction transaction) {
+        this.processTransaction(transaction.getOrigin(), transaction.getDestination(), transaction.getValue());
+        return transaction;
+    }
+
+    private RewardTransaction generateDefaultRewardTransaction(String uuid) {
+        RewardTransaction defaultTransaction = new RewardTransaction();
+        defaultTransaction.setUuid(uuid);
+        return defaultTransaction;
+    }
 
     public RewardTransaction rewardStudent(Teacher teacher, String uuid, RewardTransactionRequest reward) {
         Student student = studentRepository.findById(reward.studentId()).orElseThrow(
                 () -> new EntityNotFoundException("Student with id: " + reward.studentId() + " not found")
         );
 
-        RewardTransaction transaction = rewardTransactionRepository.findByUuid(uuid).orElseGet(() -> {
-            RewardTransaction defaultTransaction = new RewardTransaction();
-            defaultTransaction.setUuid(uuid);
-            return defaultTransaction;
-        });
+        Optional<RewardTransaction> optionalTransaction = rewardTransactionRepository.findByUuid(uuid);
+        RewardTransaction transaction = optionalTransaction.map(this::rollbackTransaction).orElseGet(() -> generateDefaultRewardTransaction(uuid));
         transaction.setValue(reward.value());
         transaction.setOrigin(teacher.getAccount());
         transaction.setDestination(student.getAccount());
         transaction.setMotive(reward.motive());
 
-        return rewardTransactionRepository.save(transaction);
+        return this.processReward(transaction);
     }
 }
