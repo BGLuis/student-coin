b0cf4ec44f1e3c445dc117d82338bceb809b9d3c
jaoppb <joaopedroperes06@gmail.com>
2025-10-29 20:35:00 -0300

    fix(back): creating accounts

--- backend/src/main/java/com/student_coin/api/service/EnterpriseService.java
+++ backend/src/main/java/com/student_coin/api/service/EnterpriseService.java
@@ -2,13 +2,16 @@ package com.student_coin.api.service;
 
 import com.student_coin.api.dto.request.EnterpriseRequest;
 import com.student_coin.api.dto.response.EnterpriseResponse;
+import com.student_coin.api.entity.Account;
 import com.student_coin.api.entity.Enterprise;
 import com.student_coin.api.enums.Roles;
 import com.student_coin.api.mapper.EnterpriseListMapper;
 import com.student_coin.api.mapper.EnterpriseMapper;
 import com.student_coin.api.mapper.UpdateEnterpriseMapper;
+import com.student_coin.api.repository.AccountRepository;
 import com.student_coin.api.repository.EnterpriseRepository;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -27,10 +30,12 @@ public class EnterpriseService {
     private PasswordEncoder encoder;
 
     private EnterpriseRepository enterpriseRepository;
+    private AccountRepository accountRepository;
     private EnterpriseMapper enterpriseMapper;
     private EnterpriseListMapper listMapper;
     private UpdateEnterpriseMapper updateEnterpriseMapper;
 
+    @Transactional
     public EnterpriseResponse register(@Valid EnterpriseRequest register) {
         Enterprise enterprise = new Enterprise();
         enterprise.setEmail(register.email());
@@ -38,6 +43,10 @@ public class EnterpriseService {
         enterprise.setName(register.name());
         enterprise.setPassword(encoder.encode(register.password()));
         enterprise.setRole(Roles.ROLE_ENTERPRISE);
+
+        Account account = this.accountRepository.save(new Account());
+        enterprise.setAccount(account);
+
         Enterprise data = enterpriseRepository.save(enterprise);
         return enterpriseMapper.toEnterpriseResponse(data);
     }
