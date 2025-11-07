4f50a48a8adaf79a007afee5b8257a7bd1fabd41
jaoppb <joaopedroperes06@gmail.com>
2025-11-07 20:27:04 -0300

    docs: swagger

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -32,6 +32,7 @@ repositories {
 dependencies {
 	implementation("org.hibernate.validator:hibernate-validator")
 	implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-data-rest")
 	implementation("org.springframework.boot:spring-boot-starter-mail")
--- backend/src/main/java/com/student_coin/api/config/SecurityConfig.java
+++ backend/src/main/java/com/student_coin/api/config/SecurityConfig.java
@@ -45,6 +45,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST,
                                 "/auth/login", "/auth")
                         .permitAll()
+                        .requestMatchers(HttpMethod.GET, "/v3/api-docs", "/v3/api-docs/*", "/swagger-ui/*")
+                        .permitAll()
                         .requestMatchers(HttpMethod.PUT, "/account/reward/*")
                         .hasAnyAuthority(String.valueOf(Roles.ROLE_TEACHER))
                         .requestMatchers(HttpMethod.PUT, "/account/redeem/*")
