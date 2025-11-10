eed2ec29c9562b5e7ad32cf3b70c7d0d1968d784
Gabriel Assis de Souza <dunkrells23@gmail.com>
2025-11-10 15:58:31 -0300

    config: swagger

---
+++ backend/target/generated-sources/annotations/com/student_coin/api/mapper/EnterpriseListMapperImpl.java
@@ -0,0 +1,51 @@
+package com.student_coin.api.mapper;
+
+import com.student_coin.api.dto.response.EnterpriseResponse;
+import com.student_coin.api.entity.Enterprise;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-10-27T16:41:18+0000",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.8 (Eclipse Adoptium)"
+)
+@Component
+public class EnterpriseListMapperImpl implements EnterpriseListMapper {
+
+    @Override
+    public List<EnterpriseResponse> toEnterpriseResponse(List<Enterprise> enterprises) {
+        if ( enterprises == null ) {
+            return null;
+        }
+
+        List<EnterpriseResponse> list = new ArrayList<EnterpriseResponse>( enterprises.size() );
+        for ( Enterprise enterprise : enterprises ) {
+            list.add( enterpriseToEnterpriseResponse( enterprise ) );
+        }
+
+        return list;
+    }
+
+    protected EnterpriseResponse enterpriseToEnterpriseResponse(Enterprise enterprise) {
+        if ( enterprise == null ) {
+            return null;
+        }
+
+        Long id = null;
+        String cnpj = null;
+        String name = null;
+        String email = null;
+
+        id = enterprise.getId();
+        cnpj = enterprise.getCnpj();
+        name = enterprise.getName();
+        email = enterprise.getEmail();
+
+        EnterpriseResponse enterpriseResponse = new EnterpriseResponse( id, cnpj, name, email );
+
+        return enterpriseResponse;
+    }
+}
