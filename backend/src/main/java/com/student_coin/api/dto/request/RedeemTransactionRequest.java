613ac948e7d9c5f0896bb8e76c9aedac7e75adc5
jaoppb <joaopedroperes06@gmail.com>
2025-11-13 20:04:28 -0300

    fix(back): redeem advantage

--- backend/src/main/java/com/student_coin/api/dto/request/RedeemTransactionRequest.java
+++ backend/src/main/java/com/student_coin/api/dto/request/RedeemTransactionRequest.java
@@ -1,12 +1,7 @@
 package com.student_coin.api.dto.request;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
-public record RedeemTransactionRequest (
-        @NotNull(message = "ID da empresa é obrigatório")
-        Long advantageId
-) implements TransactionRequest{
+public record RedeemTransactionRequest(@NotNull(message = "Advantage ID is mandatory") Long advantageId)
+        implements TransactionRequest {
 }
